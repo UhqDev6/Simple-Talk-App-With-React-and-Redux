9f5bfbf2736c37e2afaabad53cfc97616ae30c3e
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2022-12-12 15:25:39 +0800

    fix thunk-func for fetch api to users and talks

--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -1,3 +1,16 @@
-/**
- * @TODO: Define all the actions (creator) that uses a combination of actions from various domain
- */
+import api from '../../utils/api';
+import { receiveTalkActionCreator } from '../talks/action';
+import { receiveUserActionCreator } from '../users/action';
+
+const asyncPopulateUserAndTalks = () => async (dispatch) => {
+  try {
+    const users = await api.getAllUsers();
+    const talks = await api.getAllTalks();
+    dispatch(receiveUserActionCreator(users));
+    dispatch(receiveTalkActionCreator(talks));
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+export { asyncPopulateUserAndTalks };
--- src/states/users/action.js
+++ src/states/users/action.js
@@ -1,23 +1,19 @@
-import { ActionType } from "../../utils/ActionType";
-import api from "../../utils/api";
+import { ActionType } from '../../utils/ActionType';
+import api from '../../utils/api';
 
-const receiveUserActionCreator = (users) => {
-  return {
-    type: ActionType.RECEIVE_USERS,
-    payload: {
-      users,
-    },
-  };
-};
+const receiveUserActionCreator = (users) => ({
+  type: ActionType.RECEIVE_USERS,
+  payload: {
+    users,
+  },
+});
 
-const asyncRegisterUser = ({ id, name, password }) => {
-  return async () => {
-    try {
-      await api.register({ id, name, password });
-    } catch (error) {
-      alert(error.message);
-    }
-  };
+const asyncRegisterUser = ({ id, name, password }) => async () => {
+  try {
+    await api.register({ id, name, password });
+  } catch (error) {
+    alert(error.message);
+  }
 };
 
 export { receiveUserActionCreator, asyncRegisterUser };
