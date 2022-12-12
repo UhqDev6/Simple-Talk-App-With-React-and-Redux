4f5c925a9972f47bd15b668928137ee17cc5877b
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2022-12-12 13:28:30 +0800

    fix action and reducer for talkDetail

--- src/states/talkDetail/action.js
+++ src/states/talkDetail/action.js
@@ -1,3 +1,48 @@
-/**
- * @TODO: Define all the actions (creator) for the talkDetail state
- */
+import { ActionType } from '../../utils/ActionType';
+import api from '../../utils/api';
+
+const receiveTalkDetailActionCreator = (talkDetail) => ({
+  type: ActionType.RECEIVE_TALK_DETAIL,
+  payload: {
+    talkDetail,
+  },
+});
+
+const clearTalkDetailActionCreator = () => ({
+  type: ActionType.CLEAR_TALK_DETAIL,
+});
+
+const toggleLikeTalkDetailActionCreator = (userId) => ({
+  type: ActionType.TOGGLE_LIKE_TALK_DETAIL,
+  payload: {
+    userId,
+  },
+});
+
+const asyncReceiveTalkDetail = (talkId) => async (dispatch) => {
+  dispatch(clearTalkDetailActionCreator());
+  try {
+    const talkDetail = await api.getTalkDetail(talkId);
+    dispatch(receiveTalkDetailActionCreator(talkDetail));
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const asyncToggleLikeTalkDetail = () => async (dispatch, getState) => {
+  const { authUser, talkDetail } = getState();
+  dispatch(toggleLikeTalkDetailActionCreator(authUser.id));
+  try {
+    await api.toggleLikeTalk(talkDetail.id);
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+export {
+  receiveTalkDetailActionCreator,
+  clearTalkDetailActionCreator,
+  toggleLikeTalkDetailActionCreator,
+  asyncReceiveTalkDetail,
+  asyncToggleLikeTalkDetail,
+};
