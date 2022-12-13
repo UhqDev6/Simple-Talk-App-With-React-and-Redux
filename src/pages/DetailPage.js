b3cd6bbde867ab4d9547dd2652918aaf00159b23
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2022-12-13 20:30:10 +0800

    fix detailpage

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,24 +1,30 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import TalkDetail from '../components/TalkDetail';
 import TalkItem from '../components/TalkItem';
 import TalkReplyInput from '../components/TalkReplyInput';
+import { asyncReceiveTalkDetail, asyncToggleLikeTalkDetail } from '../states/talkDetail/action';
+import { asyncAddTalk } from '../states/talks/action';
 
 function DetailPage() {
   const { id } = useParams();
-  const { talkDetail = null, authUser } = {}; // @TODO: get talkDetail and authUser state from store
-  const dispatch = null; // @TODO: get dispatch function from store
+  const { talkDetail = null, authUser } = useSelector((states) => states);
+  const dispatch = useDispatch(); // @TODO: get dispatch function from store
 
   useEffect(() => {
     // @TODO: dispatch async action to get talk detail by id
+    dispatch(asyncReceiveTalkDetail(id));
   }, [id, dispatch]);
 
   const onLikeTalk = () => {
     // @TODO: dispatch async action to toggle like talk detail
+    dispatch(asyncToggleLikeTalkDetail());
   };
 
   const onReplyTalk = (text) => {
     // @TODO: dispatch async action to add reply talk
+    dispatch(asyncAddTalk({ text, replyTo: id }));
   };
 
   if (!talkDetail) {
