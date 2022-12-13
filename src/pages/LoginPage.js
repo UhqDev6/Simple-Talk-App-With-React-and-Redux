64befd7ff8fd9f411fe734eb56c7f226a5710a61
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2022-12-13 17:40:17 +0800

    fix LoginPage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { IoEarthOutline } from 'react-icons/io5';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import LoginInput from '../components/LoginInput';
+import { asyncSetAuthUser } from '../states/authUser/action';
 
 function LoginPage() {
-  const dispatch = null; // @TODO: get dispatch function from store
+  const dispatch = useDispatch(); // @TODO: get dispatch function from store
 
   const onLogin = ({ id, password }) => {
     // @TODO: dispatch async action to login
+    dispatch(asyncSetAuthUser({ id, password }));
   };
 
   return (
