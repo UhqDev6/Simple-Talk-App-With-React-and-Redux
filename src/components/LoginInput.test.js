21612f93b4ac811a407aafabd0ae15fd8e53b528
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-01-09 21:49:03 +0800

    added testing to loginPage

---
+++ src/components/LoginInput.test.js
@@ -0,0 +1,60 @@
+/**
+ * skenario testing
+ *
+ * - LoginInput component
+ *  - should handle username typing correctly
+ *  - should handle password typing correctly
+ *  - should call login function when login button is clicked
+ */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import LoginInput from './LoginInput';
+
+import '@testing-library/jest-dom';
+
+describe('LoginInput component', () => {
+  it('should handle username typing correctly', async () => {
+    // arrange
+    render(<LoginInput login={() => {}} />);
+    const usernameInput = await screen.getByPlaceholderText('Username');
+
+    // action
+    await userEvent.type(usernameInput, 'usernametest');
+
+    // assert
+    expect(usernameInput).toHaveValue('usernametest');
+  });
+
+  it('should handle password typing correctly', async () => {
+    // arrange
+    render(<LoginInput login={() => {}} />);
+    const passwordInput = await screen.getByPlaceholderText('Password');
+
+    // action
+    await userEvent.type(passwordInput, 'passwordtest');
+
+    // assert
+    expect(passwordInput).toHaveValue('passwordtest');
+  });
+
+  it('should call login function when login button is clicked', async () => {
+    // arrange
+    const mockLogin = jest.fn();
+    render(<LoginInput login={mockLogin} />);
+    const usernameInput = await screen.getByPlaceholderText('Username');
+    await userEvent.type(usernameInput, 'usernametest');
+    const passwordInput = await screen.getByPlaceholderText('Password');
+    await userEvent.type(passwordInput, 'passwordtest');
+    const loginButton = await screen.getByRole('button', { name: 'Login' });
+
+    // action
+    await userEvent.click(loginButton);
+
+    // assert
+    expect(mockLogin).toBeCalledWith({
+      id: 'usernametest',
+      password: 'passwordtest',
+    });
+  });
+});
